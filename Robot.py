a166080c183ce465d4ed153b86f4deb13af5c8e9
Smyran <smyranmath@gmail.com>
2025-12-20 18:40:53 -0700

    Changed Robot representation to use predetermined DH Parameters.

--- Robot.py
+++ Robot.py
@@ -1,39 +1,28 @@
 import numpy as np
 
+import Utility
+
 class Robot():
     
-    def __init__(self, link1, link2, link3):
+    def __init__(self):
         """
         Creates the Robot to simulate.
-        Each joint in the robot is a rotary joint.
-        Each link parameter determines the length of the corresponding link.
-
-        Parameters:
-            link1 (float): Length of Link 1
-            link2 (float): Length of Link 2
-            link3 (float): Length of Link 3
         """
-        self.link1 = link1
-        self.link2 = link2
-        self.link3 = link3
 
-        total_link_length = link1 + link2 + link3
+        d = np.array([0, 0, 0])
+        theta = np.array([0, 0, 0])
+        a = np.array([0, 2, 2])
+        alpha = np.array([np.pi/2, 0, 0])
 
-        self.zeroconfig = np.array([
-            [1, 0, 0, total_link_length],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
-        ])
+        frames = 3
+        zero_config = np.eye(4)
+
+        for i in range(frames):
+            pass
 
     def FK(self, joint1, joint2, joint3):
         """
         Returns the Homogeneous Transformation of the current pose.
-
-        Parameters:
-            joint1 (float): Degree of Joint 1
-            joint2 (float): Degree of Joint 2
-            joint3 (float): Degree of Joint 3
         """
 
     def IK(self):
