fa0a4496ccdff19f48b3e640b5798e07b71dde45
Smyran <smyranmath@gmail.com>
2025-12-20 18:40:23 -0700

    Added Translate method.

--- Utility.py
+++ Utility.py
@@ -1,12 +1,25 @@
 import numpy as np
 import numpy.typing as npt
 
+def Translate(D: npt.NDArray):
+    """
+    Returns a transformation matrix with the given 1x3 displacement vector.
+    """
+    T = np.array([
+        [0, 0, 0, D[0]],
+        [0, 0, 0, D[1]],
+        [0, 0, 0, D[2]],
+        [0, 0, 0, 1]
+    ])
+
+    return T
+
 def SSM(K: npt.NDArray):
     """
-    Converts the given 3x1 vector into a skew-symmetric matrix.
+    Converts the given 1x3 vector into a skew-symmetric matrix.
 
     Parameters:
-        K (Numpy Array): 3x1 vector to transform
+        K (Numpy Array): 1x3 vector to transform
 
     Returns:
         S (Numpy Array): 3x3 skew-symmetric matrix of the given vector.
