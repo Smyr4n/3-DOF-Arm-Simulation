70dfbb588be6bca6b4397c1f7a05b6451e6a4c84
Smyran <smyranmath@gmail.com>
2026-01-04 12:18:27 -0700

    Fixed bug where Translate was not using an identity matrix.

--- Utility.py
+++ Utility.py
@@ -26,9 +26,9 @@ def Translate(D: npt.NDArray) -> npt.NDArray:
     Returns a transformation matrix with the given 1x3 displacement vector.
     """
     T = np.array([
-        [0, 0, 0, D[0]],
-        [0, 0, 0, D[1]],
-        [0, 0, 0, D[2]],
+        [1, 0, 0, D[0]],
+        [0, 1, 0, D[1]],
+        [0, 0, 1, D[2]],
         [0, 0, 0, 1]
     ])
 
