5d104ded11561dac9afbc7a78f7c5ee8aa837240
Smyran <smyranmath@gmail.com>
2026-01-04 13:35:07 -0700

    Added Read_Config function

--- Config.py
+++ Config.py
@@ -1,2 +1,19 @@
 import json
 
+def Read_Config():
+
+    try:
+        with open("Config.json", 'r') as file:
+            data = json.load(file)
+        
+        d = data["d"]
+        a = data["a"]
+        alpha = data["alpha"]
+    
+    except Exception as E:
+        print(f"ERROR: {E}")
+        d = [0, 1, 1]
+        a = [0, 2, 2]
+        alpha = [1.57, 0, 0]
+
+    return d, a, alpha
